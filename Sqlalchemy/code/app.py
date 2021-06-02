4767837739dbe60a3e3d9353be06e53b2e9fac22
Phadnis <bhagvant.phadnis@kpn.com>
2021-06-03 00:49:35 +0530

    Creating the Store Resource

--- Sqlalchemy/code/app.py
+++ Sqlalchemy/code/app.py
@@ -8,6 +8,7 @@ from flask_jwt import JWT
 from security import authenticate,identity
 from resources.User import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'     # TO know sqlalchemy, where to find db,    currenlty it is root directory
@@ -21,60 +22,10 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)   #/auth
 
-"""
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This fiels can not be blank!"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-#        for item in items:
-#            if item['name'] == name:
-#                return item
-#        return {'item' : None}, 404
-        return {'Item' : item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message':"An item with name '{}' already exists,".format(name)}, 400
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    # delete item
-
-    def delete(self,name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message':'Item deleted'}
-
-    def put(self, name):
-
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-class ItemList(Resource):
-    def get(self):
-        return {'items':item}
-
-"""
-
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
